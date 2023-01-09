6b15108008a7c6ae8a29095b5b23c86504657fa1
MargauxPuget <margaux.puget@gmail.com>
2023-01-09 11:30:11 +0100

    début de la création de la page acceuil

--- src/components/Accueil/index.js
+++ src/components/Accueil/index.js
@@ -1,15 +1,17 @@
 import './style.scss';
+import chien from 'src/assets/images/categories/categorie-chien.jpg'
 
 // == composant
 
 const Accueil = () => (
   <div className="accueil">
     <h1 className="accueil_title">Elevages Les Petits Rocks</h1>
-    <div className="accueil_menu"> Menu</div>
+    <h2 className="accueil_menu"> Menu</h2>
     <div className="accueil_description">Danish donut chocolate cake tiramisu jelly-o candy canes jujubes chocolate. Chocolate cake gummi bears marshmallow dessert soufflé biscuit. Bear claw liquorice fruitcake icing pudding sweet roll cheesecake. Cupcake chupa chups tootsie roll lemon drops candy canes fruitcake ice cream dragée bear claw. Sweet roll chocolate gummi bears carrot cake jelly beans pudding shortbread sweet roll. Oat cake shortbread jelly tart biscuit. Tiramisu cotton candy macaroon pudding candy macaroon gingerbread.</div>
     <ul className="accueil_list">
       <div className="accueil_categorie">
-        Nos chiens
+        <img className="accueil_categorie_img" src={chien} alt="{chien}" />
+        <h3 className="accueil_categorie_chiens">Nos chiens</h3>
       </div>
       <div className="accueil_categorie">
         Nos chiots
